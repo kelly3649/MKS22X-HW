7a12d77175b8cc8001d5e13cf5ad4a0fdeefebbd
Kelly Wang <kwang6@stuy.edu>
2016-02-08 22:34:38 -0500

    get the basic sqrt part without the exception or the interface

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,2 +1,15 @@
-public class Recursion implements hw01{
+import java.lang.Math;
+public class Recursion{
+    public String name(){
+	return "Wang," + "Kelly";
+    }
+    public double sqrtH(double guess, double n){
+	if((Math.abs(n - guess*guess) / ((guess*guess + n) / 2)) * 100 < 1){
+	    return guess;
+	}
+	return sqrtH((n/guess + guess)/2,n);
+    }
+    public double sqrt(double n){
+	return sqrtH(n/2,n);
+    }
 }
