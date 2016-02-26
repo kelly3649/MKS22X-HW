0ebf58cd1d1a4972653b76be979901d7665c0d16
Kelly Wang <kwang6@stuy.edu>
2016-02-25 21:49:24 -0500

    started working on concepts and scanning in

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class Bronze{
-    String s = "input";
+    
+    int numRow,numCol; //(R,C)
+    int elevation;//(E)
+    int numDir; // number of directions(N)
+    int[][] landscape;
+    int[] R_s,C_s,D_s;
+    numRow = Integer.parseInt(input.substring(0,1));
+    numCol = Integer.parseInt(input.substring(1,2));
+    landscape = new int[numRow][numCol];
+
+    public Bronze(String fileName){
+	File file = new File(fileName);
+	try{
+	   Scanner sc = new Scanner(new File("makelake.in"))){
+	while(sc.hasNext()){
+	    input += sc.next();
+	}
+    }	
+    }
     public void stomp(){
     }
-    public printBoard{
+    public void printBoard(){
+	for(int i = 0; i<numRow;i++){
+	    for(int index = 0;index<numCol;index++){
+		System.out.print(landscape[i][index]);
+	    }
+	    System.out.print("\n");
+	}
+    }
+
+    public static void main(String[]args){
+	Bronze b1 = new Bronze();
+	b1.printBoard();
     }
 }
