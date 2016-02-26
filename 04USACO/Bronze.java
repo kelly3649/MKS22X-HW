7b264b2f375ec053dd5c57e0fcf9a8adc989921e
Kelly Wang <kwang6@stuy.edu>
2016-02-26 12:57:33 -0500

    SCANNER WORKSgit add -A! :)

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -3,38 +3,44 @@ import java.io.File;
 import java.io.FileNotFoundException;
 public class Bronze{
     
-    int numRow,numCol; //(R,C)
-    int elevation;//(E)
-    int numDir; // number of directions(N)
-    int[][] landscape;
-    int[] R_s,C_s,D_s;
-    File file = new File(fileName);
-    try{
-	Scanner sc = new Scanner(new File("makelake.in"));
-	while(sc.hasNext()){
-	    input += sc.next();
-	}
-    }
-    numRow = Integer.parseInt(input.substring(0,1));
-    numCol = Integer.parseInt(input.substring(1,2));
-    landscape = new int[numRow][numCol];
+    
+    
 
-    public Bronze(String fileName){
-	
+    
+    public void solve(){
     }
-    public void stomp(){
-    }
-    public void printBoard(){
-	for(int i = 0; i<numRow;i++){
+    
+
+    public static void main(String[]args){
+	int numRow=0,numCol=0; //(R,C)
+	int elevation;//(E)
+	int numDir; // number of directions(N)
+	int[][] landscape;
+	int[] R_s,C_s,D_s;
+	String input = "";
+	try{
+	    Scanner sc = new Scanner(new File("makelake.in"));
+	    while(sc.hasNextLine()){
+		input += sc.nextLine() + "\n";
+	    }
+	}catch(FileNotFoundException e){
+	    System.out.println("NO FILE FOUND");
+	}
+
+	System.out.print(input);
+    //numRow = Integer.parseInt(input.substring(0,1));
+    //numCol = Integer.parseInt(input.substring(1,2));
+    //landscape = new int[numRow][numCol];
+	/*	for(int i = 0; i<numRow;i++){
 	    for(int index = 0;index<numCol;index++){
 		System.out.print(landscape[i][index]);
 	    }
 	    System.out.print("\n");
 	}
-    }
-
-    public static void main(String[]args){
-	Bronze b1 = new Bronze();
-	b1.printBoard();
+	
+	*/
+	
+	//b1.printBoard();
     }
 }
+   
