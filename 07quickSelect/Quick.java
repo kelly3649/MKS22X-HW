b61d9e4580f3e4cba72bb1e130e28dcacfce1089
Kelly Wang <kwang6@stuy.edu>
2016-03-07 23:39:42 -0500

    started on partition

---
+++ 07quickSelect/Quick.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Quick{
+    private static int partition(int[]data, int left, int right){
+
+	int aIndex = Random.nextInt(right-left+1) + left;
+	int a = data[aIndex];
+	
+    }
+    public static int quickselect(int[] data, int k);
+}
