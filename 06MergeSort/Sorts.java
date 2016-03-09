7de7b9418e1eb91028da9dea60e713eba0243461
Kelly Wang <kwang6@stuy.edu>
2016-03-08 21:39:11 -0500

    tested cases, IT WORKS!, cleaned up code

--- 06MergeSort/Sorts.java
+++ 06MergeSort/Sorts.java
@@ -21,9 +21,7 @@ public class Sorts{
 		data[nowI] = copy;
 		nowI -= 1;	
 	    }
-	  
 	}
-
     }
     
     public static void selectionSort(int[] data){
@@ -48,11 +46,12 @@ public class Sorts{
 		    int copy = data[index];
 		    data[index] = data[index+1];
 		    data[index+1] = copy;
-		}//printArray(data);
-	    }printArray(data);
+		}
+	    }//printArray(data);
 	}
     }
-    public static void merge(int[] data, int startA, int endA, int startB, int endB){System.out.println("endB/startB" + endB + "/" + startB + endA + "/" + startA);
+    public static void merge(int[] data, int startA, int endA, int startB, int endB){
+	//System.out.println("endB/startB" + endB + "/" + startB + endA + "/" + startA);
 	
 	int[] newAry = new int[data.length];
 	int cntrA = startA;
@@ -76,25 +75,14 @@ public class Sorts{
 		newAry[i+startA] = data[cntrB];
 		cntrB++;
 	    }
-	    printArray(newAry);
+	    //	    printArray(newAry);
 	    i++;
 	}
-	/*if(cntrA == endA+1){
-	    for(int i = cntrB;i<endB+1;i++){
-		newAry[i] = data[i];
-	    }
-	}
-	else{for(int i = cntrA;i<endA+1;i++){
-		newAry[i] = data[i];
-	    }
-	    }*/
 	//copy it over
-	printArray(newAry);
 	for(int index = startA;index<endB+1;index++){
-		//		System.out.println(newList.get(i));
 	    data[index] = newAry[index];
 	}
-	printArray(data);
+	//printArray(data);
     }
 
     public static void mergesortH(int[] data,int start, int end){
@@ -106,9 +94,6 @@ public class Sorts{
 	    mergesortH(data,1 + start + (end-start)/2,end);
 	    merge(data,start,start + (end-start)/2,1 + start + (end-start)/2,end);
 	}
-
-	//System.out.println("data length is" + data.length + "start/end" + start + "/" + end);
-	
     }
 
     public static String name(){
@@ -120,16 +105,14 @@ public class Sorts{
     public static void main(String[]args){
 	System.out.println(name());
 	int[] ary = {0,1,-3,4};
-	int[] ary2 = {-5,2,5,6};
-		merge(ary2,0,1,2,3);
-	
-	//	mergesort(ary);
-	printArray(ary2);
+	int[] ary2 = {-5,2,5,6};	
+	mergesort(ary);
+	printArray(ary);
 	int[] arey = {-1,-3,-2,-2,-2,5,10,1};
 	int[] arey2 = {-5,3,1,4,6,-1};
-	mergesort(arey2);
-	printArray(arey2);
-	//	System.out.println(1/2);
+	mergesort(arey);
+	printArray(arey);
     }
 }
 
+//IT WORKS OMG >.<
