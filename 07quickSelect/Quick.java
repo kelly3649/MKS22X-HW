ab7a05dee4ac062530dbf84a18314f5f8e9e0393
Kelly Wang <kwang6@stuy.edu>
2016-03-15 22:36:30 -0400

    started working on new quickSort but don't really understand...

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -1,7 +1,81 @@
 import java.util.Random;
 
 public class Quick{
-    private static int partition(int[]data, int left, int right){
+    private static int partitionOld(int[]data, int left, int right){
+	Random r = new Random();
+	int aIndex = r.nextInt(right-left+1) + left;
+	int a = data[aIndex];
+	//System.out.println("a/index" + a +"/" + aIndex);
+	int strtR = right;
+	//put "a" in corner, like a problem child
+	int copy = data[strtR];
+	data[strtR] = a;
+	data[aIndex] = copy;
+	//printArray(data);
+	right--;
+	while(left!= right){
+	    //System.out.println("left/right" + left +"/" + right);
+	    if(data[left] <= a){
+		left++;
+	    }
+	    else{
+		int temp=data[left];
+		data[left] = data[right];
+		data[right] = temp;
+		right--;
+	    }
+	    //printArray(data);
+	    //	    System.out.println("left/right" + left + "/" + right);
+	}
+	//place "a" back, but remember to check that what it swapped with
+	if(data[left] < a){
+	    int temp2 = data[left+1];
+	    data[left+1] = a;
+	    data[strtR] = temp2;
+	    //printArray(data);
+	    return left+1;
+	}
+	else{
+	    int temp3 = data[left];
+	    data[left] = a;
+	    data[strtR] = temp3;
+	    //  printArray(data);
+	    return left;
+	}		
+    }
+    public static int quickselectOld(int[] data, int k){
+	return quickselectOld(data,k,0,data.length-1);	
+    }
+    private static int quickselectOld(int[] data, int k, int left, int right){
+	if(left==right){
+	    return data[left];
+	}
+	int retNum = partitionOld(data,0,data.length-1);
+	if(retNum == k){ //if you guessed correctly, and it happened to be k
+	    return data[k];
+	}
+	else{
+	    if(retNum < k){
+		return quickselectOld(data,k,retNum+1,right);
+	    }
+	    else{
+		return quickselectOld(data,k,left,retNum-1);
+	    }
+	}
+    }
+    public static void quickSortOld(int[] data){
+	quickSortOld(data,0,data.length-1);
+    }
+    private static void quickSortOld(int[] data, int left, int right){
+	//System.out.println("left/right" + left + "/" + right);
+	if(right-left>=1){
+	    int retNum = partition(data,left,right);
+	    //printArray(data);
+	    quickSortOld(data,left,retNum-1);
+	    quickSortOld(data,retNum+1,right);
+	}
+    }
+    private static int[] partition(int[]data, int left, int right){
 	Random r = new Random();
 	int aIndex = r.nextInt(right-left+1) + left;
 	int a = data[aIndex];
