0765c4b129561517e7c372a5e9bfecb4ccf80f2b
Kelly Wang <kwang6@stuy.edu>
2016-04-08 00:10:51 -0400

    am confused on how to start Deque...

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MyDeque<T>{
+    MyLinkedList<T> m = new MyLinkedList<T>();
+    
+    private int size;
+    private int start;
+    private int end;
+
+    void addLast(e){
+    }
+    T removeFirst(){
+    }
+
+}
